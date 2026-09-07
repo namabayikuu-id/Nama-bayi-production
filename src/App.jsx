342f8945a203106da9559fcd0876fca392b5e4be
namabayikuu-id <namabayikuu.id@gmail.com>
2026-09-07 20:26:56 +0700

    Set crossOrigin property for image loading

--- src/App.jsx
+++ src/App.jsx
@@ -111,6 +111,7 @@ function drawContent(ctx, type, data, S) {
 function loadImgFromUrl(url) {
   return new Promise(resolve => {
     const img = new Image();
+    img.crossOrigin = "anonymous";
     img.onload = () => resolve(img);
     img.onerror = () => resolve(null);
     img.src = url;
